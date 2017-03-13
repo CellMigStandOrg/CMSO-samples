69e8c5435b51967b2784b386fbe870440f3ee0b8
simleo <simleo@crs4.it>
2017-03-13 15:34:05 +0000

    add_hcs_metadata: fixes and updates

--- scripts/add_hcs_metadata.py
+++ scripts/add_hcs_metadata.py
@@ -2,20 +2,21 @@ import sys
 import argparse
 import xml.dom.minidom as minidom
 
-
+N_ROWS = 6
+N_COLUMNS = 8
 WELL_MAP = {
-    (1, 1): "9I5TT808_F00000010.tif",
-    (2, 1): "9I5TT808_F00000011.tif",
-    (3, 1): "9I5TT808_F00000012.tif",
-    (4, 1): "9I5TT808_F00000013.tif",
-    (1, 3): "9I5TT808_F00000014.tif",
-    (2, 3): "9I5TT808_F00000015.tif",
-    (3, 3): "9I5TT808_F00000016.tif",
-    (4, 3): "9I5TT808_F00000017.tif",
-    (1, 5): "9I5TT808_F00000018.tif",
-    (2, 5): "9I5TT808_F00000019.tif",
-    (3, 5): "9I5TT808_F00000020.tif",
-    (4, 5): "9I5TT808_F00000021.tif",
+    (1, 1): ("9I5TT808_F00000010.tif", "p190,control"),
+    (2, 1): ("9I5TT808_F00000011.tif", "p190,control"),
+    (3, 1): ("9I5TT808_F00000012.tif", "p190,y27632"),
+    (4, 1): ("9I5TT808_F00000013.tif", "p190,y27632"),
+    (1, 3): ("9I5TT808_F00000014.tif", "p210,control"),
+    (2, 3): ("9I5TT808_F00000015.tif", "p210,control"),
+    (3, 3): ("9I5TT808_F00000016.tif", "p210,y27632"),
+    (4, 3): ("9I5TT808_F00000017.tif", "p210,y27632"),
+    (1, 5): ("9I5TT808_F00000018.tif", "p210m,control"),
+    (2, 5): ("9I5TT808_F00000019.tif", "p210m,control"),
+    (3, 5): ("9I5TT808_F00000020.tif", "p210m,y27632"),
+    (4, 5): ("9I5TT808_F00000021.tif", "p210m,y27632"),
 }
 INDENT = 3 * " "
 
@@ -29,21 +30,24 @@ def map_image_names(doc):
 
 
 def add_wells(doc, plate, img_map):
-    for (row, col), fn in sorted(WELL_MAP.iteritems()):
+    for (row, col), (fn, well_type) in sorted(WELL_MAP.iteritems()):
+        well_idx = row * N_COLUMNS + col
         try:
             img_id = img_map[fn]
         except KeyError:
             continue
-        well_id = "Well:%d:%d" % (row, col)
         image_ref = doc.createElement("ImageRef")
         image_ref.setAttribute("ID", img_id)
         well_sample = doc.createElement("WellSample")
-        well_sample.setAttribute("ID", "%s:0" % well_id)
+        # only one WellSample per Well
+        well_sample.setAttribute("ID", "WellSample:%d:%d:0" % (row, col))
+        well_sample.setAttribute("Index", "%d" % well_idx)
         well_sample.appendChild(image_ref)
         well = doc.createElement("Well")
-        well.setAttribute("ID", well_id)
+        well.setAttribute("ID", "Well:%d:%d" % (row, col))
         well.setAttribute("Row", "%d" % row)
         well.setAttribute("Column", "%d" % col)
+        well.setAttribute("Type", well_type)
         well.appendChild(well_sample)
         plate.appendChild(well)
 
@@ -63,6 +67,8 @@ def main(argv):
     plate = doc.createElement("Plate")
     plate.setAttribute("ID", "Plate:0")
     plate.setAttribute("Name", "Ba/F3")
+    plate.setAttribute("Rows", "%d" % N_ROWS)
+    plate.setAttribute("Columns", "%d" % N_COLUMNS)
     add_wells(doc, plate, img_map)
     root = doc.firstChild
     root.appendChild(plate)
